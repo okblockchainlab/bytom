9c9e3e5e2eca94fffa116ff00c5d36a4e61fb048
fatcat22 <pandayt@163.com>
2018-08-29 17:33:28 +0800

    createrawtransaction&signrawtransaction test success

--- okwallet/okwallet/wallet.go
+++ okwallet/okwallet/wallet.go
@@ -1,48 +1,278 @@
 package okwallet
 
 import (
-  "encoding/hex"
-  "fmt"
-  "errors"
+	"context"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
 
-  "github.com/bytom/crypto/ed25519/chainkd"
-  "github.com/bytom/account"
+	"github.com/bytom/account"
+	"github.com/bytom/api"
 	"github.com/bytom/blockchain/signers"
+	"github.com/bytom/blockchain/txbuilder"
 	"github.com/bytom/common"
-	"github.com/bytom/crypto"
 	"github.com/bytom/consensus"
+	"github.com/bytom/crypto"
+	"github.com/bytom/crypto/ed25519/chainkd"
+	btm_json "github.com/bytom/encoding/json"
+	btm_errors "github.com/bytom/errors"
+	"github.com/bytom/protocol/bc"
+	"github.com/bytom/protocol/bc/types"
 )
+
+const (
+	defaultTxTTL = 5 * time.Minute
+)
+
+type OKUTXO struct {
+	OutputID            bc.Hash    `json:"id"`
+	SourceID            bc.Hash    `json:"source_id"`
+	AssetID             bc.AssetID `json:"asset_id"`
+	Amount              uint64     `json:"amount"`
+	SourcePos           uint64     `json:"source_pos"`
+	ControlProgram      []byte     `json:"program"`
+	AccountID           string     `json:"account_id"`
+	Address             string     `json:"address"`
+	ControlProgramIndex uint64     `json:"control_program_index"`
+	ValidHeight         uint64     `json:"valid_height"`
+	Change              bool       `json:"change"`
+}
+
+type OKAction struct {
+	Type        string
+	Transaction OKUTXO
+}
+
+type OKBuildRequest struct {
+	Tx *types.TxData `json:"base_transaction"`
+	//Actions   []OKAction    `json:"actions"`
+	Actions   []map[string]interface{} `json:"actions"`
+	XPub      chainkd.XPub             `json:xpub`
+	TTL       btm_json.Duration        `json:"ttl"`
+	TimeRange uint64                   `json:"time_range"`
+}
+
+type GetAddressResp struct {
+	XPub    string `json:xpub`
+	Address string `json:address`
+}
+
+type signResp struct {
+	Tx           *txbuilder.Template `json:"transaction"`
+	SignComplete bool                `json:"sign_complete"`
+}
+
+type spendUTXOAction struct {
+	UTXO *account.UTXO
+	XPub *chainkd.XPub
+}
+
+func (a *spendUTXOAction) Build(_ context.Context, b *txbuilder.TemplateBuilder) error {
+	accountSigner, err := createSigner([]chainkd.XPub{*a.XPub})
+	if err != nil {
+		return err
+	}
+
+	txInput, sigInst, err := account.UtxoToInputs(accountSigner, a.UTXO)
+	if err != nil {
+		return err
+	}
+
+	return b.AddInput(txInput, sigInst)
+}
+
+func createSigner(xpubs []chainkd.XPub) (*signers.Signer, error) {
+	return signers.Create("account", xpubs, 1, 1)
+}
+
+func okUTXO2UTXO(oktx *OKUTXO) *account.UTXO {
+	return &account.UTXO{
+		OutputID:            oktx.OutputID,
+		SourceID:            oktx.SourceID,
+		AssetID:             oktx.AssetID,
+		Amount:              oktx.Amount,
+		SourcePos:           oktx.SourcePos,
+		ControlProgram:      oktx.ControlProgram,
+		AccountID:           oktx.AccountID,
+		Address:             oktx.Address,
+		ControlProgramIndex: oktx.ControlProgramIndex,
+		ValidHeight:         oktx.ValidHeight,
+		Change:              oktx.Change,
+	}
+}
+
+func decodeSpendUTXOAction(data []byte, xpub *chainkd.XPub) (txbuilder.Action, error) {
+	var okact OKAction
+	if err := json.Unmarshal(data, &okact); err != nil {
+		return nil, fmt.Errorf("unmarshal OKAction error. %v", err)
+	}
+
+	a := &spendUTXOAction{okUTXO2UTXO(&okact.Transaction), xpub}
+	return a, nil
+}
+
+func decodeControlAddressAction(data []byte, _ *chainkd.XPub) (txbuilder.Action, error) {
+	return txbuilder.DecodeControlAddressAction(data)
+}
+
+func decodeControlProgramAction(data []byte, _ *chainkd.XPub) (txbuilder.Action, error) {
+	return txbuilder.DecodeControlProgramAction(data)
+}
+
+func decodeRetireAction(data []byte, _ *chainkd.XPub) (txbuilder.Action, error) {
+	return txbuilder.DecodeRetireAction(data)
+}
+
+func actionDecoder(action string) (func([]byte, *chainkd.XPub) (txbuilder.Action, error), bool) {
+	decoders := map[string]func([]byte, *chainkd.XPub) (txbuilder.Action, error){
+		"control_address": decodeControlAddressAction,
+		"control_program": decodeControlProgramAction,
+		//"issue":                        a.wallet.AssetReg.DecodeIssueAction,
+		"retire": decodeRetireAction,
+		//"spend_account":                a.wallet.AccountMgr.DecodeSpendAction,
+		"spend_account_unspent_output": decodeSpendUTXOAction,
+	}
+	decoder, ok := decoders[action]
+	return decoder, ok
+}
 
 //reference: API.createAccount, API.createAccountReceiver
 func GetAddressByPrivateKey(prvkeyStr string) (string, error) {
-  prvkeyBytes, err := hex.DecodeString(prvkeyStr)
-  if err != nil {
-    return "", fmt.Errorf("decoding private key error: %v", err)
-  }
-
-  var xprv chainkd.XPrv
-  if len(xprv) != len(prvkeyBytes) {
-    return "", errors.New("invalid private key")
-  }
-  copy(xprv[:], prvkeyBytes)
-
-  xpub := xprv.XPub()
-
-  const quorum = 1
-	signer, err := signers.Create("account", []chainkd.XPub{xpub}, quorum, 1)
-	id := signers.IDGenerate()
+	prvkeyBytes, err := hex.DecodeString(prvkeyStr)
 	if err != nil {
-		return "", fmt.Errorf("generate id for account failed: %v", err)
+		return "", fmt.Errorf("decoding private key error: %v", err)
 	}
 
-	account := &account.Account{Signer: signer, ID: id, Alias: "noused"}
+	var xprv chainkd.XPrv
+	if len(xprv) != len(prvkeyBytes) {
+		return "", errors.New("invalid private key")
+	}
+	copy(xprv[:], prvkeyBytes)
 
-  const idx = 1
+	xpub := xprv.XPub()
+
+	const quorum = 1
+	signer, err := createSigner([]chainkd.XPub{xpub})
+	if err != nil {
+		return "", err
+	}
+
+	account := &account.Account{Signer: signer, ID: signers.IDGenerate(), Alias: "noused"}
+
+	const idx = 1
 	path := signers.Path(account.Signer, signers.AccountKeySpace, idx)
 	derivedXPubs := chainkd.DeriveXPubs(account.XPubs, path)
 	derivedPK := derivedXPubs[0].PublicKey()
 	pubHash := crypto.Ripemd160(derivedPK)
 
 	address, err := common.NewAddressWitnessPubKeyHash(pubHash, &consensus.ActiveNetParams)
-  return address.EncodeAddress(), nil
+
+	getAddrResp := GetAddressResp{XPub: hex.EncodeToString(xpub[:]), Address: address.EncodeAddress()}
+	result, err := json.Marshal(getAddrResp)
+	return string(result), nil
+}
+
+//reference: API.build
+// action{type, output_id}, {amount, asset_id, address, type}, ttl, time_range
+func CreateRawTransaction(reqStr string) (string, error) {
+	var req OKBuildRequest
+	if err := json.Unmarshal([]byte(reqStr), &req); err != nil {
+		return "", err
+	}
+
+	actions := make([]txbuilder.Action, 0, len(req.Actions))
+	for i, act := range req.Actions {
+		typ, ok := act["type"].(string)
+		if !ok {
+			return "", btm_errors.WithDetailf(api.ErrBadActionType, "no action type provided on action %d", i)
+		}
+
+		decoder, ok := actionDecoder(typ)
+		if !ok {
+			return "", errors.New("unknown action type " + typ)
+		}
+
+		// Remarshal to JSON, the action may have been modified when we
+		// filtered aliases.
+		b, err := json.Marshal(act)
+		if err != nil {
+			return "", err
+		}
+
+		action, err := decoder(b, &req.XPub)
+		if err != nil {
+			return "", btm_errors.WithDetailf(api.ErrBadAction, "%s on action %d", err.Error(), i)
+		}
+
+		actions = append(actions, action)
+	}
+	actions = account.MergeSpendAction(actions)
+
+	ttl := req.TTL.Duration
+	if ttl == 0 {
+		ttl = defaultTxTTL
+	}
+	maxTime := time.Now().Add(ttl)
+
+	tpl, err := txbuilder.Build(nil, req.Tx, actions, maxTime, req.TimeRange)
+	if btm_errors.Root(err) == txbuilder.ErrAction {
+		// append each of the inner btm_errors contained in the data.
+		var Errs string
+		var rootErr error
+		for i, innerErr := range btm_errors.Data(err)["actions"].([]error) {
+			if i == 0 {
+				rootErr = btm_errors.Root(innerErr)
+			}
+			Errs = Errs + innerErr.Error()
+		}
+		err = btm_errors.WithDetail(rootErr, Errs)
+	}
+	if err != nil {
+		return "", err
+	}
+
+	// ensure null is never returned for signing instructions
+	if tpl.SigningInstructions == nil {
+		tpl.SigningInstructions = []*txbuilder.SigningInstruction{}
+	}
+
+	result, err := json.Marshal(tpl)
+	return string(result), err
+}
+
+func pseudohsmSignTemplate(_ context.Context, _ chainkd.XPub, path [][]byte, data [32]byte, prvkeyStr string) ([]byte, error) {
+	prvkeyBytes, err := hex.DecodeString(prvkeyStr)
+	if err != nil {
+		return nil, fmt.Errorf("decoding private key error: %v", err)
+	}
+
+	var xprv chainkd.XPrv
+	if len(xprv) != len(prvkeyBytes) {
+		return nil, errors.New("invalid private key")
+	}
+	copy(xprv[:], prvkeyBytes)
+
+	if len(path) > 0 {
+		xprv = xprv.Derive(path)
+	}
+	return xprv.Sign(data[:]), nil
+}
+
+//reference: API.pseudohsmSignTemplates
+func SignRawTransaction(prvkeyStr, templateStr string) (string, error) {
+	var tpl txbuilder.Template
+	if err := json.Unmarshal([]byte(templateStr), &tpl); err != nil {
+		return "", err
+	}
+
+	var unused context.Context
+	if err := txbuilder.Sign(unused, &tpl, prvkeyStr, pseudohsmSignTemplate); err != nil {
+		return "", fmt.Errorf("failed on sign transactio. %v", err)
+	}
+
+	resp := signResp{Tx: &tpl, SignComplete: txbuilder.SignProgress(&tpl)}
+	result, err := json.Marshal(resp)
+	return string(result), err
 }
--- okwallet/okwallet/wallet_test.go
+++ okwallet/okwallet/wallet_test.go
@@ -1,28 +1,81 @@
 package okwallet
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestGetAddressByPrivateKey(t *testing.T) {
-  prvkey1 := `98b94afc04a4dcff39e63bf48d01da8c6057f2564b35def6210add648316584510fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a`
-  const expect1 = `bm1qjvj4hy7tkycp99yuv6u9xjl5n0l0n3uwtzesf6`
-  prvkey2 := `50024008ed99dfe4680e695fa0467c2960d1a8efbe206d986ba763611288674b9216b09d23f8c96d7ead0309fd53db524194d0a871a0ff718eac85308ffd7415`
-  const expect2 = `bm1qecew0p8lrftfcc0l8sdppyk07fwk9gpcvnr6e8`
+	prvkey1 := `98b94afc04a4dcff39e63bf48d01da8c6057f2564b35def6210add648316584510fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a`
+	const expect1 = `{"XPub":"f7bf3b320f828566dff61ec222d4cdbcea032892c8c542b3b6a1c3c725ec356210fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a","Address":"bm1qjvj4hy7tkycp99yuv6u9xjl5n0l0n3uwtzesf6"}`
+	prvkey2 := `50024008ed99dfe4680e695fa0467c2960d1a8efbe206d986ba763611288674b9216b09d23f8c96d7ead0309fd53db524194d0a871a0ff718eac85308ffd7415`
+	const expect2 = `{"XPub":"8b833f46f53b67e9ee87ca8be70e75da5edf91cfc6c427615ad9d6d96e3ec4e19216b09d23f8c96d7ead0309fd53db524194d0a871a0ff718eac85308ffd7415","Address":"bm1qecew0p8lrftfcc0l8sdppyk07fwk9gpcvnr6e8"}`
 
-  addr, err := GetAddressByPrivateKey(prvkey1)
-  if err != nil {
-    t.Fatal(err)
-  }
-  if expect1 != addr {
-    t.Fatal("GetAddressByPrivateKey failed. expect " + expect1 + " but return " + addr)
-  }
+	addr, err := GetAddressByPrivateKey(prvkey1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expect1 != addr {
+		t.Fatal("GetAddressByPrivateKey failed. expect " + expect1 + " but return " + addr)
+	}
 
-  addr, err = GetAddressByPrivateKey(prvkey2)
-  if err != nil {
-    t.Fatal(err)
-  }
-  if expect2 != addr {
-    t.Fatal("GetAddressByPrivateKey failed. expect " + expect2 + " but return " + addr)
-  }
+	addr, err = GetAddressByPrivateKey(prvkey2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expect2 != addr {
+		t.Fatal("GetAddressByPrivateKey failed. expect " + expect2 + " but return " + addr)
+	}
+}
+
+func TestCreateRawTransaction(t *testing.T) {
+	const inputs = `
+	{
+		"base_transaction":null,
+		"actions":[
+			{
+				"type":"spend_account_unspent_output",
+				"transaction":{
+					"account_alias": "default",
+					"account_id": "0BKBR2D2G0A02",
+					"address": "bm1qx7ylnhszg24995d5e0nftu9e87kt9vnxcn633r",
+					"amount": 624000000000,
+					"asset_alias": "BTM",
+					"asset_id": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+					"change": false,
+					"control_program_index": 12,
+					"id": "5af9d3c9b69470983377c1fc0c9125c4ac3bfd32c8d505f2a6042aade8503bc9",
+					"program": "00143789f9de0242aa52d1b4cbe695f0b93facb2b266",
+					"source_id": "233d1dd49e591980f98e11f333c6c28a867e78448e272011f045131df5aa260b",
+					"source_pos": 0,
+					"valid_height": 12
+				}
+			}
+		],
+		"ttl":0,
+		"time_range":0,
+		"xpub": "f7bf3b320f828566dff61ec222d4cdbcea032892c8c542b3b6a1c3c725ec356210fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a"
+	}`
+	const expect = `{"raw_transaction":"07010001016c016a233d1dd49e591980f98e11f333c6c28a867e78448e272011f045131df5aa260bffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80c0b1ca9412000121d34d78dfbf3d7fd75ed36e3669ae767756f871b7baf797f46fdddf69c6f66f6eba010000","signing_instructions":[{"position":0,"witness_components":[{"type":"raw_tx_signature","quorum":1,"keys":[{"xpub":"f7bf3b320f828566dff61ec222d4cdbcea032892c8c542b3b6a1c3c725ec356210fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a","derivation_path":["010100000000000000","0c00000000000000"]}],"signatures":null},{"type":"data","value":"a42b4998ff44281e6594c7e83b055bd7324a99f5fa27169f5cb5f92e6bb7d922"}]}],"allow_additional_actions":false}`
+
+	rawTx, err := CreateRawTransaction(inputs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expect != rawTx {
+		t.Fatal("CreateRawTransaction failed. expect " + expect + " but return " + rawTx)
+	}
+}
+
+func TestSignRawTransaction(t *testing.T) {
+	const prvkey = `98b94afc04a4dcff39e63bf48d01da8c6057f2564b35def6210add648316584510fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a`
+	const rawTx = `{"raw_transaction":"07010001016c016a233d1dd49e591980f98e11f333c6c28a867e78448e272011f045131df5aa260bffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80c0b1ca9412000121d34d78dfbf3d7fd75ed36e3669ae767756f871b7baf797f46fdddf69c6f66f6eba010000","signing_instructions":[{"position":0,"witness_components":[{"type":"raw_tx_signature","quorum":1,"keys":[{"xpub":"f7bf3b320f828566dff61ec222d4cdbcea032892c8c542b3b6a1c3c725ec356210fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a","derivation_path":["010100000000000000","0c00000000000000"]}],"signatures":null},{"type":"data","value":"a42b4998ff44281e6594c7e83b055bd7324a99f5fa27169f5cb5f92e6bb7d922"}]}],"allow_additional_actions":false}`
+	const expect = `{"transaction":{"raw_transaction":"07010001016c016a233d1dd49e591980f98e11f333c6c28a867e78448e272011f045131df5aa260bffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80c0b1ca9412000121d34d78dfbf3d7fd75ed36e3669ae767756f871b7baf797f46fdddf69c6f66f6eba63024021b0d16ed75ac59b29eafed2a9881694bfdada041683cb21bb31e4942095e9fc646eefe27b455cb97ff312e8248ae318a1b344dab25d262437975536d60dac0d20a42b4998ff44281e6594c7e83b055bd7324a99f5fa27169f5cb5f92e6bb7d92200","signing_instructions":[{"position":0,"witness_components":[{"type":"raw_tx_signature","quorum":1,"keys":[{"xpub":"f7bf3b320f828566dff61ec222d4cdbcea032892c8c542b3b6a1c3c725ec356210fd3d91011aeba9d41f1654c0c8232d1f94eb807a89a35e1501c4effb5c4d5a","derivation_path":["010100000000000000","0c00000000000000"]}],"signatures":["21b0d16ed75ac59b29eafed2a9881694bfdada041683cb21bb31e4942095e9fc646eefe27b455cb97ff312e8248ae318a1b344dab25d262437975536d60dac0d"]},{"type":"data","value":"a42b4998ff44281e6594c7e83b055bd7324a99f5fa27169f5cb5f92e6bb7d922"}]}],"allow_additional_actions":false},"sign_complete":true}`
+
+	resp, err := SignRawTransaction(prvkey, rawTx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expect != resp {
+		t.Fatal("SignRawTransaction failed. expect " + expect + " but return " + resp)
+	}
 }
